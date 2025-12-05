b317f4d99e4ab6f2905c71f6fd27c5ab17fdad21
Yos1706 <yoshuatoro@gmail.com>
2025-12-04 23:15:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema'); 
 
+const { schema, root } = require('./schema/schema'); 
+
+const mockData = require('./MOCK_DATA.json'); 
 
 const app = express();
 
 app.use(cors());
 
 app.get('/rest/getAllUsers', (req, res) => {
-     res.json(mockData); 
-    
+    res.json(mockData);
+   
 });
 
 app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true, 
+    schema: schema,
+    rootValue: root, 
+    graphiql: true,
 }));
 
 const PORT = 3000;
@@ -24,4 +27,5 @@ const HOST = '0.0.0.0';
 app.listen(PORT, HOST, () => {
     console.log(`Server running on http://${HOST}:${PORT}`);
     console.log(`REST endpoint: http://${HOST}:${PORT}/rest/getAllUsers`);
+    console.log(`GraphQL endpoint: http://${HOST}:${PORT}/graphql`);
 });
